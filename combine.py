8687c39df15d54dd96f1a334a55e7f5f1ada1108
NingAnMe <ninganme@qq.com>
2019-05-09 14:27:05 +0800

    完成植被指数的合成程序

--- combine.py
+++ combine.py
@@ -8,9 +8,41 @@ from __future__ import print_function
 
 import os
 import numpy as np
+import sys
 
 from hdf5 import write_hdf5_and_compress
+from initialize import load_yaml_file
 from load import LoadH8Ndvi
+
+
+def main(yaml_file):
+    """
+    :param yaml_file: (str) 接口yaml文件
+    :return:
+    """
+    # ######################## 初始化 ###########################
+    # 加载接口文件
+    print("main: interface file <<< {}".format(yaml_file))
+
+    interface_config = load_yaml_file(yaml_file)
+    i_in_files = interface_config['PATH']['ipath']  # 待处理文件绝对路径（list）
+    i_out_file = interface_config['PATH']['opath']  # 输出文件绝对路径（str）
+
+    in_files = list()
+    for in_file in i_in_files:
+        if os.path.isfile(in_file):
+            in_files.append(in_file)
+        else:
+            print('***WARNING***File is not existent: {}'.format(in_file))
+
+    file_count = len(in_files)
+    if len(in_files) <= 0:
+        print('###ERROR###The count of Valid files is 0')
+        return
+    else:
+        print('---INFO---File count: {}'.format(file_count))
+
+    combine(in_files, i_out_file)
 
 
 def combine(in_files, out_file):
@@ -44,10 +76,22 @@ def combine(in_files, out_file):
     write_hdf5_and_compress(out_file, result)
 
 
-if __name__ == '__main__':
-    in_dir = r'D:\KunYu\hangzhou_anning\H8_NDVI'
-    out_dir = r'D:\KunYu\hangzhou_anning\H8_NDVI_COMBINE'
-    in_files = [os.path.join(in_dir, i) for i in os.listdir(in_dir)]
+# ######################## 程序全局入口 ##############################
+if __name__ == "__main__":
+    # 获取程序参数接口
+    ARGS = sys.argv[1:]
+    HELP_INFO = \
+        u"""
+        [arg1]：yaml_path
+        [example]： python app.py arg1
+        """
+    if "-h" in ARGS:
+        print(HELP_INFO)
+        sys.exit(-1)
 
-    out_file = os.path.join(out_dir, 'NDVI_COMBINE_AHI8_OBI_2000M_NOM_20190427.hdf')
-    combine(in_files, out_file)
+    if len(ARGS) != 1:
+        print(HELP_INFO)
+        sys.exit(-1)
+    else:
+        ARG1 = ARGS[0]
+        main(ARG1)
