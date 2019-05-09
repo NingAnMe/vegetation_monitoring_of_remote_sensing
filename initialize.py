ffb65a12b92568762050efe0f85e54066a2917cf
NingAnMe <ninganme@qq.com>
2019-05-09 14:26:18 +0800

    增加读取yaml文件的函数

--- initialize.py
+++ initialize.py
@@ -4,3 +4,10 @@
 @Time    : 2019/5/9
 @Author  : AnNing
 """
+import yaml
+
+
+def load_yaml_file(in_file):
+    with open(in_file, 'r') as stream:
+        config_data = yaml.load(stream)
+    return config_data
