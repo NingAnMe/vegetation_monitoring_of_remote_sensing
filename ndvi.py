e1f443fa58c0a55efcd78f89add3fbb45ecb5873
NingAnMe <ninganme@qq.com>
2019-05-09 14:08:02 +0800

    修改计算植被指数的算法为通用处理

--- ndvi.py
+++ ndvi.py
@@ -6,22 +6,24 @@
 """
 from __future__ import print_function
 
-import os
 import numpy as np
 
-from load import LoaderH8L1
-from hdf5 import write_hdf5_and_compress
 
+def cal_ndvi(r_vis, r_nir, t_tir):
+    """
+    计算植被指数 ndvi
 
-def cal_ndvi(in_file, out_file):
+    :param r_vis:  可见光：无效值使用nan填充
+    :param r_nir:  近红外：无效值使用nan填充
+    :param t_tir:  亮温：无效值使用nan填充
+
+    :return: ndvi, flag
+    flag:
+    0: 植被指数
+    1： 云判别
+    2： 水体判别
+    3： 无效遥感数据
     """
-    计算H8的植被指数
-    :return:
-    """
-    loder = LoaderH8L1(in_file, res=2000)
-    r_vis = loder.get_vis()
-    r_nir = loder.get_nir()
-    t_tir = loder.get_tir()
 
     ndvi = (r_nir - r_vis) / (r_nir + r_vis)  # 计算植被指数
 
@@ -47,17 +49,4 @@ def cal_ndvi(in_file, out_file):
     flag[water] = 2  # 水体的判别标识
     flag[np.isnan(ndvi)] = 3  # 无效值标识
 
-    # 写HDF5文件
-    result = {'NDVI': ndvi, 'Flag': flag}
-    write_hdf5_and_compress(out_file, result)
-
-
-if __name__ == '__main__':
-    in_dir = r'D:\KunYu\hangzhou_anning\H8_L1'
-    out_dir = r'D:\KunYu\hangzhou_anning\H8_NDVI'
-    for filename in os.listdir(in_dir):
-        if '2000' not in filename:
-            continue
-        in_file = os.path.join(in_dir, filename)
-        out_file = os.path.join(out_dir, 'NDVI_' + filename)
-        cal_ndvi(in_file, out_file)
+    return ndvi, flag
